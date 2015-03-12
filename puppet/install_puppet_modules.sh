e0e15cafb10ee1a66fce7a084a9562e35ed431fd
Gavin Noronha <gnoronha@financialengines.com>
2015-03-11 17:02:47 -0700

    removed git name setup

---
+++ puppet/install_puppet_modules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit on any errors.
+set -e
+
+PUPPET_INSTALL='puppet module install --module_repository http://forge.puppetlabs.com'
+
+# install puppet modules
+(puppet module list | grep acme-ohmyzsh) ||
+    $PUPPET_INSTALL -v 0.1.2 acme-ohmyzsh
+
+(puppet module list | grep garethr-docker) ||
+    $PUPPET_INSTALL -v 2.2.0 garethr-docker
--- puppet/manifests/classes/init.pp
+++ puppet/manifests/classes/init.pp
@@ -14,16 +14,16 @@ class init {
             package { $misc_packages:
                 ensure => present,
             }
-            exec { 'setup-git-user':
-                command => "$git_cmd user.name $::git_name",
-                environment => "HOME=/home/vagrant",
-                require => Package['git-core'],
-            } 
-            exec { 'setup-git-email':
-                command => "$git_cmd user.email $::git_email",
-                environment => "HOME=/home/vagrant",
-                require => Package['git-core']
-            } 
+#            exec { 'setup-git-user':
+#                command => "$git_cmd user.name $::git_name",
+#                environment => "HOME=/home/vagrant",
+#                require => Package['git-core'],
+#            } 
+#            exec { 'setup-git-email':
+#                command => "$git_cmd user.email $::git_email",
+#                environment => "HOME=/home/vagrant",
+#                require => Package['git-core']
+#            }
         }
     }
 }
