07c3c64ac216a98b3d022d7a8528200b950d4ea1
Gavin Noronha <gavinln@hotmail.com>
2015-06-01 19:22:12 -0700

    added example for celery-rabbitmq

---
+++ kubernetes/celery-rabbitmq/execute.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# kubectl is in the parent directory
+KUBECTL=$DIR/../kubectl
+
+# $KUBECTL create -f $DIR/rabbitmq-service.yaml
+
+# $KUBECTL create -f $DIR/rabbitmq-controller.yaml
+# $KUBECTL create -f $DIR/celery-controller.yaml
+# $KUBECTL create -f $DIR/flower-controller.yaml
+
+
