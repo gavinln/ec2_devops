48ce6b074b132fead96c9978b72b290b4bcf8ec7
Gavin Noronha <gavinln@hotmail.com>
2015-06-02 21:57:07 -0700

    added public ip

--- kubernetes/nginx_run.sh
+++ kubernetes/nginx_run.sh
@@ -4,4 +4,4 @@
 ./kubectl run-container nginx --image=nginx --port=80
 
 # expose nginx as a service
-./kubectl expose rc nginx --port=80
+./kubectl expose rc nginx --port=80 --public-ip=192.168.33.10
