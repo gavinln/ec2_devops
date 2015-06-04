51d27007c8e69fcfceef7610e450e4ebf5300119
Gavin Noronha <gavinln@hotmail.com>
2015-06-03 22:21:13 -0700

    fetching the kubectl and bash completions

--- puppet/manifests/classes/kubernetes_setup.pp
+++ puppet/manifests/classes/kubernetes_setup.pp
@@ -6,6 +6,18 @@ class kubernetes_setup {
     docker::image { 'gcr.io/google_containers/hyperkube':
         image_tag => 'v0.17.0'
     }
+    wget::fetch { 'kubectl':
+        source => "https://storage.googleapis.com/kubernetes-release/release/v0.17.0/bin/linux/amd64/kubectl",
+        destination => "/tmp/kubectl"
+    } ->
+    file { '/usr/bin/kubectl':
+        mode => 0755,
+        source => '/tmp/kubectl'
+    } ->
+    wget::fetch { 'kubectl_bash':
+        source => "https://raw.githubusercontent.com/GoogleCloudPlatform/kubernetes/4ba22e713ad95dfb308d0b9aca7ac9d980414bf6/contrib/completions/bash/kubectl",
+        destination => "/etc/bash_completion.d/kubectl"
+    }
 }
 
 
