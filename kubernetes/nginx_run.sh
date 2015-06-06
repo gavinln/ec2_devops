37dc94e88707b8cfb4298743882986db83bb625f
Gavin Noronha <gavinln@hotmail.com>
2015-06-06 01:17:58 -0700

    now using kubectl installed centrally

--- kubernetes/nginx_run.sh
+++ kubernetes/nginx_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run nginx application
-./kubectl run-container nginx --image=nginx --port=80
+kubectl run-container nginx --image=nginx --port=80
 
 # expose nginx as a service
-./kubectl expose rc nginx --port=80 --public-ip=192.168.33.10
+kubectl expose rc nginx --port=80 --public-ip=192.168.33.10
