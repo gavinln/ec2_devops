6d3a64e38473b87846cc5a791ab3839ae89924d5
Gavin Noronha <gavinln@hotmail.com>
2015-09-07 21:14:27 -0700

    added recursive example

---
+++ haskell/recursive_functions.hs
@@ -0,0 +1,20 @@
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+  | x > maxTail = x
+  | otherwise = maxTail
+  where maxTail = maximum' xs
+
+maximum2' :: (Ord a) => [a] -> a
+maximum2' [] = error "maximum of empty list"
+maximum2' [x] = x
+maximum2' (x:xs) = max x (maximum2' xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x:replicate' (n - 1) x
+
+
