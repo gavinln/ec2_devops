dbe68d805444ae32a9406d478def812fc1741317
Gavin Noronha <gnoronha@financialengines.com>
2015-10-22 08:50:07 -0700

    updated docker-compose file

--- docker/grafana/influx_shell.sh
+++ docker/grafana/influx_shell.sh
@@ -6,10 +6,4 @@ if [ -z "$INFLUX_DOCKER" ]; then
     exit 1
 fi
 
-echo "type Ctrl+d to exit"
-echo "CREATE DATABASE site"
-echo "USE site"
-echo "CREATE USER grafana WITH PASSWORD 'somepassword' WITH ALL PRIVILEGES"
-echo "GRANT ALL PRIVILEGES ON site TO grafana"
-
 docker exec -t -i $INFLUX_DOCKER /opt/influxdb/influx
